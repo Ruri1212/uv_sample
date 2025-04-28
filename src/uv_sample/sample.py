7ff5a392c7e87e0f60cbad1311db6bbf2c6962a3
Ruri1212 <4624502@ed.tus.ac.jp>
2025-04-28 12:16:43 +0900

    feat: loggerの出力を追加

--- src/uv_sample/sample.py
+++ src/uv_sample/sample.py
@@ -1,12 +1,16 @@
 import numpy as np
 
+from src.library import RichLogger
+
 
 def add(a: np.ndarray, b: np.ndarray) -> int:
     return a + b
 
 
 if __name__ == "__main__":
+    save_dir = "data/output"
+    logger = RichLogger.get_logger(save_dir)
     a = np.array([1, 2, 3])
     b = np.array([4, 5, 6])
     result = add(a, b)
-    print("Result:", result)
+    logger.info("Result: %s", result)
